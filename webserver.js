c6fdeb8855018bdcfb2766f3f91a62c31cb61bcb
Mallamin <mallamin@yahoo.com>
2020-04-05 00:35:32 +0200

    outputting json

--- webserver.js
+++ webserver.js
@@ -2,23 +2,22 @@
 var http = require("http");
 var fs = require("fs");
 
-http.createServer(function(req, res) {
-  /*outputting plain text
+http
+  .createServer(function (req, res) {
+    /*outputting plain text
   res.writeHead(200, { "content-Type": "text/plain" });*/
     //res.writeHead(200, { "content-Type": "text/html" });
     // outputting JASON
     res.writeHead(200, { "content-Type": "application/json" });
-    var person={firstName:"Mohamed Lamin",
+    var person = {
+      firstName: "Mohamed Lamin",
       lastName: "Jalloh",
       countryOfOrigin: "The Federal Republic of Africa",
-      Age: "Why is it so important?"
-    
-
+      Age: "Why is it so important?",
     };
-   /*converting fs.readFileSync into a string by adding a char encoding
+    /*converting fs.readFileSync into a string by adding a char encoding
    
    var html = fs.readFileSync(__dirname + "/web.html","utf8");*/
-    
 
     /*create a message var
 
@@ -26,6 +25,6 @@ http.createServer(function(req, res) {
 
     // using the replace method on the str
     html= html.replace("{Message}", message);*/
-    res.end(JSON.strigify(person));
-  }
-}).listen(1337, "127.0.0.1");
+    res.end(JSON.stringify(person));
+  })
+  .listen(1337, "127.0.0.1");
